37f46ac3dc2a9a3b52c6bd9d3610f9b6b393efd2
wajahat5 <t-wajahat.yusuf@ocltp.com>
2026-03-03 15:01:05 +0530

    integrated deployed be

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
+const shouldUseLocalBackend = false
 export const config = {
   // Toggle between mock APIs (true) and real backend APIs (false)
   shouldUseMockAPIs: false,
 
   // Optional: base URL for real backend APIs when shouldUseMockAPIs is false
-  apiBaseUrl: "http://localhost:3000"
+  apiBaseUrl: shouldUseLocalBackend?"http://localhost:3000": "https://tea-garden-project.vercel.app",
 };
